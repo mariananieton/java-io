b5fa83270060046ae06a225e4ca62d1452816414
mariananieton <rm94141@fiap.com.br>
2023-01-10 11:26:41 -0300

    Testando métodos de input e output

--- src/br/com/alura/java/io/teste/TesteCopiarArquivo.java
+++ src/br/com/alura/java/io/teste/TesteCopiarArquivo.java
@@ -1,24 +1,28 @@
 package br.com.alura.java.io.teste;
 
 import java.io.*;
+import java.net.Socket;
 
 public class TesteCopiarArquivo {
 
 	public static void main(String[] args) throws IOException {
 
-		InputStream fis = new FileInputStream("lorem.txt");
+		//Socket s = new Socket();
+
+		InputStream fis = System.in; //new FileInputStream("lorem.txt"); //s.getInputStream();
 		Reader isr = new InputStreamReader(fis);
 		BufferedReader br = new BufferedReader(isr);
 
-		OutputStream fos = new FileOutputStream("lorem2.txt");
+		OutputStream fos = System.out; //new FileOutputStream("lorem2.txt"); //s.getOutputStream();
 		Writer osw = new OutputStreamWriter(fos);
 		BufferedWriter bw = new BufferedWriter(osw);
 
 		String linha = br.readLine();
 
-		while(linha != null) {
+		while(linha != null && !linha.isEmpty()) {
 			bw.write(linha);
 			bw.newLine();
+			bw.flush();
 			linha = br.readLine();
 		}
 
