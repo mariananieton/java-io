9bcaac5c89ff19f02eecb0b6b7c8ef899f587aff
mariananieton <rm94141@fiap.com.br>
2023-01-10 12:06:07 -0300

    Utilizando FileWriter

---
+++ src/br/com/alura/java/io/teste/TesteEscrita2.java
@@ -0,0 +1,21 @@
+package br.com.alura.java.io.teste;
+
+import java.io.*;
+
+public class TesteEscrita2 {
+
+	public static void main(String[] args) throws IOException {
+
+		/*OutputStream fos = new FileOutputStream("lorem2.txt");
+		Writer osw = new OutputStreamWriter(fos);
+		BufferedWriter bw = new BufferedWriter(osw);*/
+
+		BufferedWriter bw = new BufferedWriter(new FileWriter("lorem2.txt"));
+		bw.write("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod ");
+		bw.newLine();
+		bw.newLine();
+		bw.write("asdasddasd asgfdgf sdfsdf s");
+
+		bw.close();
+	}
+}
